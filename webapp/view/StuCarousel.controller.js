87ee4c56435ae4b895699602acf2c8cd8640f4ce
xiaowangren <wangren_0626@126.com>
2016-03-29 15:50:11 +0800

    first commit

---
+++ webapp/view/StuCarousel.controller.js
@@ -0,0 +1,13 @@
+// sap.ui.define([
+// 	"sap/ui/core/mvc/Controller"
+// ], function(Controller) {
+// 	"use strict";
+
+// 	return Controller.extend("com.stu.pro.view.StuCarousel", {
+
+// 	});
+
+// });
+sap.ui.controller("com.stu.pro.view.StuCarousel", {
+	
+});
